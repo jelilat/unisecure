91e788818e9848255b79d54f3bcda7073836048d
Jelilat Anofiu <anofiujelailah@gmail.com>
2023-06-11 05:08:52 +0100

    update dummy data

--- apis/src/entities.ts
+++ apis/src/entities.ts
@@ -13,25 +13,28 @@ const wallet = new Wallet(privateKey);
 
 const hospitalData = {
   // Dummy data
-  '0x123': { name: 'Alice', bloodType: 'A', conditions: ['Condition1', 'Condition2'] },
-  '0x456': { name: 'Bob', bloodType: 'B', conditions: ['Condition3', 'Condition4'] }
+  '0x66fe4806cd41bcd308c9d2f6815aef6b2e38f9a3': { name: 'Alice', bloodType: 'A', conditions: ['Condition1', 'Condition2'] },
+  '0xc41672e349c3f6dadf8e4031b6d2d3d09de276f9': { name: 'Bob', bloodType: 'B', conditions: ['Condition3', 'Condition4'] },
+  '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2': { name: 'Charlie', bloodType: 'O', conditions: ['Condition5', 'Condition6'] }
 };
 
 const bankData = {
   // Dummy data
-  '0x123': { name: 'Alice', balance: 1000 },
-  '0x456': { name: 'Bob', balance: 2000 }
+  '0x66fe4806cd41bcd308c9d2f6815aef6b2e38f9a3': { name: 'Alice', balance: 1000 },
+  '0xc41672e349c3f6dadf8e4031b6d2d3d09de276f9': { name: 'Bob', balance: 2000 },
+  '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2': { name: 'Charlie', balance: 3000 }
 };
 
 const universityData = {
   // Dummy data
-  '0x123': { name: 'Alice', degree: 'Computer Science', GPA: 3.8 },
-  '0x456': { name: 'Bob', degree: 'Electrical Engineering', GPA: 3.9 }
+  '0x66fe4806cd41bcd308c9d2f6815aef6b2e38f9a3': { name: 'Alice', degree: 'Computer Science', GPA: 3.8 },
+  '0xc41672e349c3f6dadf8e4031b6d2d3d09de276f9': { name: 'Bob', degree: 'Electrical Engineering', GPA: 3.9 },
+  '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2': { name: 'Charlie', degree: 'Mechanical Engineering', GPA: 3.7 }
 };
 
 async function handleRequest(req: Request, res: Response, dataObject: any) {
     const address = req.params.address;
-    const originalData = dataObject[address];
+    const originalData = dataObject[address.toLowerCase()];
 
     // Convert data to string
     const dataString = JSON.stringify(originalData);
